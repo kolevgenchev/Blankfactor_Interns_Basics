d83488829946ba6c43a8cda6e89412581b82ed67
momchilslavov1 <131446908+momchilslavov1@users.noreply.github.com>
2023-04-26 12:37:29 +0300

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,20 +1,23 @@
-function fizzBuzz(num){
+/*
+ * Java Program to Print 1 to 15 Numbers using For Loop
+ */
 
-    let res = []
-
-    for(let i = 1; i <= num; i++){
-
-    if(i % 3 === 0 && i % 5 === 0){
-        res.push('FizzBuzz')
-    } else if (i % 3 === 0){
-        res.push('Fizz')
-    } else if (i % 5 === 0){
-        res.push('Buzz')
-    } else {
-        res.push(i)
+public class For_Loop
+{
+    public static void main(String[] args)
+    {
+        int n = 15;
+        for (int i = 1; i <= 15; i++)
+        {
+            if( n % 3 == 0 && n % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            //System.out.println(i);
+            else if ( n % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (n % 5 == 0)
+                Console.WriteLine("Buzz");
+        }
     }
-    }
-console.log(res);
 }
-
-fizzBuzz(16)

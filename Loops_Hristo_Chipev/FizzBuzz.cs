c24da27b70be679891963c636ddc2cfe939072b4
HristoChipev <hristo.chipev@blankfactor.com>
2023-04-27 09:07:04 +0300

    initial commit

---
+++ Loops_Hristo_Chipev/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CSharp_Tasks
+{
+	public class FizzBuzz
+    {
+        public static string[] fizzBuzz(int n)
+        {
+            string[] arr = new string[n];
+            string res = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((i + 1) % 3 == 0) res += "Fizz";
+                if ((i + 1) % 5 == 0) res += "Buzz";
+
+                if (res != "") arr[i] = res;
+                else arr[i] = (i + 1).ToString();
+                res = "";
+            }
+
+            return arr;
+        }
+        
+	}
+}
+

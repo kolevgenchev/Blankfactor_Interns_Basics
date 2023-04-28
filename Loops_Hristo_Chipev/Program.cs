1d282b4c9c0b9245affff7a841cbbac4c3942531
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-04-28 18:07:56 +0300

    updated branch

---
+++ Loops_Hristo_Chipev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using CSharp_Tasks;
+
+// Join two strings zipper style
+Console.WriteLine(JoinStrs.JoinStringsM("abc", "rtqwwe"));
+
+// Palindrome
+Console.WriteLine(Palindrome.PalindromeM("racecar"));
+Console.WriteLine(Palindrome.Palindrome2M(121));
+
+// Fizz Buzz
+Array.ForEach(FizzBuzz.FizzBuzzM(15), Console.WriteLine);
+
+// Check the sum
+Console.WriteLine(CheckSum.CheckSumM("abc", "cba", "cdb"));

753d3c5a75be8820c0c625a1439d41961984e2d9
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-04-28 18:06:45 +0300

    updated branch

---
+++ Loops_MilchoKasmetov/CheckTheSum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckTheSum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input Word One");
+        var inputOne = Console.ReadLine().ToCharArray();
+        Console.WriteLine("Input Word Two");
+        var inputTwo = Console.ReadLine().ToCharArray();
+        Console.WriteLine("Target Result");
+        var result = Console.ReadLine().ToCharArray();
+
+        Console.WriteLine("Is equal ???");
+        Console.WriteLine(IsEqual(inputOne,inputTwo, result));
+
+    }
+
+    public static bool IsEqual(char[] inputWordOne, char[] inputWordTwo, char[] result)
+    {
+
+
+        return result.Sum(x => x - 97) == inputWordOne.Sum(x => x - 97) + inputWordTwo.Sum(x => x - 97);
+    }
+}
+

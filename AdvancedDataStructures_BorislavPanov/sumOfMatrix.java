47e5b3d4514ae5c750c3ed805fef7045a0c43823
Borislav Panov <borislav.panov@blankfactor.com>
2023-04-29 16:19:22 +0300

    Create sumOfMatrix.java

---
+++ AdvancedDataStructures_BorislavPanov/sumOfMatrix.java
@@ -0,0 +1,13 @@
+public class sumOfMatrix {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1,2,3}, {4,5,6}};
+        int[][] matrix2 = {{7,8,9}, {10,11,12}};
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                System.out.print(matrix1[i][j] + matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

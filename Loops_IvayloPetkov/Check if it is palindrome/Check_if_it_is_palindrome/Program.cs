4c51599c49c91dfca5f8e3c65c2efff9f6587a5b
Petkoww <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-01 23:27:29 +0300

    Bug Fix

--- Loops_IvayloPetkov/Check if it is palindrome/Check_if_it_is_palindrome/Program.cs
+++ Loops_IvayloPetkov/Check if it is palindrome/Check_if_it_is_palindrome/Program.cs
@@ -1,15 +1,38 @@
-﻿static void palendrome()
+﻿using System;
+using System.Text;
+
+namespace Application
 {
-    string x = Console.ReadLine();
-    int a1 = x.Length;
-    if (x[a1 - 1] == x[0])
+    class Program
     {
-        Console.WriteLine("True");
-    }
-    else
-    {
-        Console.WriteLine("False");
+
+        static void check_if_it_is_palindrome()
+        {
+            string x = Console.ReadLine();
+            int x_length = x.Length;
+            StringBuilder result = new StringBuilder();
+
+            for (int i = x_length - 1; i >= 0; i--)
+            {
+                result.Append(x[i]);
+            }
+
+            if (result.ToString() == x )
+            {
+                Console.WriteLine("True");
+            }
+
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            check_if_it_is_palindrome();
+        }
     }
 }
-
-palendrome();
--- Loops_IvayloPetkov/Merge two strings/Merge_Two_Strings/Program.cs
+++ Loops_IvayloPetkov/Merge two strings/Merge_Two_Strings/Program.cs
@@ -1,6 +1,27 @@
-﻿string string1 = "abc";
-string string2 = "pqr";
-for (int i = 0; i < string1.Length || i < string2.Length; i++)
+﻿using System;
+using System.Text;
+
+namespace Application
 {
-    Console.Write(string1[i]); Console.Write(string2[i]);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = Console.ReadLine();
+            string string2 = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < string1.Length || i < string2.Length; i++)
+            {
+
+                if (i < string1.Length)
+                    result.Append(string1[i]);
+
+                if (i < string2.Length)
+                    result.Append(string2[i]);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
 }

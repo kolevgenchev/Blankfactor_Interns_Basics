d77470fcf406607f04e5fb2c776ae2b1fb811569
aleksandararsov <aleksandar.arsov@blankfactor.com>
2023-05-02 09:42:39 +0300

    DataStructures_Aleksandar

---
+++ DataStructures_Aleksandar_Arsov/PhoneBook/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections;
+
+//PHONEBOOK TASK 2 OF DATA STRUCTURES
+
+//Suppose you are developing a phone book application, where users can store and retrieve phone numbers for their contacts.
+//    Phone numbers will be represented as strings. Choose which data structure you will use to store the phone numbers.
+//    Add at least 6 phone numbers. Add phone number: ‘+359 83 74 84 84’ at 4th position.Print in the console all phones.
+//    Remove the phone in the 3rd position. Print the phone book again.
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        List<string> phones = new List<string>();
+        phones.Add("+359 88 12 32 12");
+        phones.Add("+359 89 54 64 75");
+        phones.Add("+359 89 09 04 43");
+        phones.Add("+359 88 42 44 12");
+        phones.Add("+359 90 89 88 12");
+        phones.Add("+359 88 65 64 73");
+
+        Console.WriteLine("Phonebook:\n");
+
+        for (int i = 0; i < phones.Count; i++)
+        {
+            Console.WriteLine(phones[i]);
+        }
+
+        phones.Insert(3, "+359 83 74 84 84");
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Phonebook after adding phone number at 4-th position: \n");
+
+        for (int i = 0; i < phones.Count; i++)
+        {
+
+            Console.WriteLine(phones[i]);
+        }
+
+        phones.RemoveAt(2);
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Phonebook after removing phone number in third position: \n");
+
+        for (int i = 0; i < phones.Count; i++)
+        {
+
+            Console.WriteLine(phones[i]);
+        }
+    }
+}
+
+
+
---
+++ DataStructures_Aleksandar_Arsov/Printer/Program.cs
@@ -0,0 +1,57 @@
+﻿//PRINTER TASK 1 OF DATA STRUCTURES
+//The printer in the office has two types of paper: plain and glossy, which are represented by the numbers 0 and 1, respectively.
+//    Employees start submitting tasks to the printer. Each job is specifying the type of paper it requires.
+//The number of papers in the printer is equal to the number of tasks.
+//Before the execution of each task:
+//If it prefers the paper type that is currently available, the task will be executed.
+//Otherwise, the paper remains unused and ready for the next task. And the current task will be placed as last and will wait for all others to be done
+//so it can try again.
+//This continues until none of the tasks want to take the top paper and are thus unable to print the document.
+//Write a function that accepts two parameters: an array of tasks and an array of papers’ type and returns the number
+//    of tasks that are unable to finish.
+
+class Program
+{
+    public static int CountUnfinishedTasks(int[] tasks, int[] papers)
+    {
+        int numTasks = tasks.Length;
+        int numPapers = papers.Length;
+        int numUnfinished = 0;
+        int topPaperIndex = 0;
+
+        for (int i = 0; i < numTasks; i++)
+        {
+            int paperType = tasks[i];
+
+
+            if (papers[topPaperIndex] == paperType)
+            {
+                topPaperIndex++;
+                if (topPaperIndex == numPapers)
+                {
+
+                    topPaperIndex = 0;
+                }
+            }
+            else
+            {
+
+                numUnfinished++;
+            }
+        }
+
+        return numUnfinished;
+    }
+    static void Main(string[] args)
+    {
+
+        int[] tasks = { 1, 1, 0, 0, 0, 0 };
+        int[] papers = { 0, 0, 0, 0, 0, 0 };
+
+        int numUnfinished = CountUnfinishedTasks(tasks, papers);
+
+        Console.WriteLine($"Number of unfinished tasks: {numUnfinished}");
+    }
+
+}
+

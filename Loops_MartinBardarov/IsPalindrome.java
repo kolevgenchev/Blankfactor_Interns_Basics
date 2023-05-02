3268a767ec071b7e375550c4d83ecbeab87caabc
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-02 09:39:37 +0300

    Fixing

--- Loops_MartinBardarov/IsPalindrome.java
+++ Loops_MartinBardarov/IsPalindrome.java
@@ -10,4 +10,19 @@ public class IsPalindrome {
         }
         return reversed;
     }
+
+    public boolean isPalindromeInteger(int number){
+        boolean reversed = false;
+        StringBuilder input1 = new StringBuilder();
+        String reversedNum= Integer.toString(number);
+        for(int i=reversedNum.length()-1;i>=0;i--) {
+            input1.append(reversedNum.charAt(i));
+        }
+        System.out.println(input1);
+        System.out.println(reversedNum);
+            if (input1.toString().equals(reversedNum)){
+                reversed=true;
+        }
+            return reversed;
+    }
 }

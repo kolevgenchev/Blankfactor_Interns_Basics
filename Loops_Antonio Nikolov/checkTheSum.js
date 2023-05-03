f50bc5b3d46178a6467bdead0cc00235ee493bc9
AntonioNik <antonio.nikolov@blankfactor.com>
2023-05-03 09:48:39 +0300

    small changes

--- Loops_Antonio Nikolov/checkTheSum.js
+++ Loops_Antonio Nikolov/checkTheSum.js
@@ -1,17 +1,15 @@
 const checkSum = (firstWord, secondWord, targetWord) => {
-  const charCodeSum = (str) => {
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] !== "a") {
-        sum += str[i].charCodeAt(0) - 97;
-      }
-    }
-    return sum;
-  };
-
   return (
     charCodeSum(firstWord) + charCodeSum(secondWord) === charCodeSum(targetWord)
   );
 };
-
+const charCodeSum = (str) => {
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "a") {
+      sum += str[i].charCodeAt(0) - 97;
+    }
+  }
+  return sum;
+};
 console.log(checkSum("bbb", "bbb", "bbb"));
--- Loops_Antonio Nikolov/palindromeCheck.js
+++ Loops_Antonio Nikolov/palindromeCheck.js
@@ -1,15 +1,10 @@
 const palindromeCheck = (input) => {
+  //parsing input to string
+  const parsedInput = input.toString();
+  //string to arr, reversing the arr, back to str
+  const reversedInput = parsedInput.split("").reverse().join("");
 
-    //parsing input to string
-const parsedInput = input.toString();
-    //string to arr, reversing the arr, back to str
-const reversedInput = parsedInput.split('').reverse().join('');
-    
-if(parsedInput === reversedInput){
-    return true
-}else{
-    return false
-}
-}
+  return parsedInput === reversedInput;
+};
 
-console.log(palindromeCheck(121))
+console.log(palindromeCheck(121));

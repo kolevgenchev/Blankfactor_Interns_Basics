b2abe9253caac8f170d0ee4c0091dbab840e5fd1
HristoChipev <hristo.chipev@blankfactor.com>
2023-05-03 13:57:04 +0300

    OOP Encapsulation finished exercises

---
+++ OOP-Encapsulation_HristoChipev/Program.cs
@@ -0,0 +1,20 @@
+﻿using OOP_Encapsulation;
+
+var student = new Student("Hristo", 18, new string[] { });
+
+Console.WriteLine(student.Name);
+Console.WriteLine(student.Age);
+student.AddCourse("Maths");
+student.AddCourse("Bel");
+
+foreach (var course in student.GetCourses())
+{
+    Console.WriteLine(course);
+}
+Console.WriteLine("-------------");
+student.RemoveCourse("Bel");
+
+foreach (var course in student.GetCourses())
+{
+    Console.WriteLine(course);
+}
---
+++ OOP-Encapsulation_HristoChipev/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace OOP_Encapsulation
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+        private List<string> courses;
+
+        public Student(string name, int age, string[] courses)
+        {
+            this.name = name;
+            this.age = age;
+            this.courses = new List<string>(courses);
+        }
+
+        public void AddCourse(string course)
+        {
+            courses.Add(course);
+        }
+
+        public void RemoveCourse(string course)
+        {
+            courses.Remove(course);
+        }
+
+        public string[] GetCourses()
+        {
+            return courses.ToArray();
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+        }
+    }
+}
+

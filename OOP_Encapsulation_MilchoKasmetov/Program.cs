29751f1b813959088ea7e56e535b33265bcdc4d7
Milcho Kasmetov <milcho.kasmetov@blankfactor.com>
2023-05-03 15:07:31 +0300

    OOP Encapsulation MilchoKasmetov

---
+++ OOP_Encapsulation_MilchoKasmetov/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading.Channels;
+
+namespace OOP_Encapsulation_MilchoKasmetov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("How many students ?");
+                    var n = int.Parse(Console.ReadLine());
+
+                    var students = new List<Student>();
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Write name");
+
+                        var name = Console.ReadLine().Trim();
+
+                        Console.WriteLine("Write Age");
+
+                        var age = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Creating student");
+                        var student = new Student(name, age);
+
+                        Console.WriteLine("Student created");
+
+                        while (true)
+                        {
+                            Console.WriteLine("Press Key");
+                            Console.WriteLine("1 Add course");
+                            Console.WriteLine("2 Remove course");
+                            Console.WriteLine("0 Exit");
+
+                            var input = int.Parse(Console.ReadLine().Split().First());
+
+                            if (input == 0)
+                            {
+                                break;
+                            }
+
+                            switch (input)
+                            {
+                                case 1:
+                                    Console.WriteLine("Name of course");
+
+                                    var courseToAdd = Console.ReadLine().ToLower();
+
+                                    student.AddCourse(courseToAdd);
+
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Name of course");
+
+                                    var courseToRemove = Console.ReadLine().ToLower();
+
+                                    student.RemoveCourse(courseToRemove);
+
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid input option !");
+                                    break;
+                            }
+
+                        }
+
+                        students.Add(student);
+                    }
+                    Console.WriteLine(new string('-', 100));
+                    students.ForEach(student => 
+                    Console.WriteLine($"Student name: {student.GetName()}, Student age: {student.GetAge()}, Courses -> {(student.Courses.Count != 0 ? string.Join(", ", student.Courses) : "None :(")}"));
+
+                    Console.WriteLine(new string('-', 100));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"ERROR TRY AGAIN :( {e.Message}");
+                }
+            }
+
+        }
+    }
+}
---
+++ OOP_Encapsulation_MilchoKasmetov/Student.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OOP_Encapsulation_MilchoKasmetov
+{
+    public class Student
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 120;
+
+        private string _name;
+        private int _age;
+
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Courses = new List<string>();
+        }
+
+
+        private string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name cannot be empty.");
+                }
+
+
+                this._name = value;
+            }
+        }
+
+        private int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    throw new Exception($"Age should be between {MinAge} and {MaxAge}.");
+                }
+
+
+                this._age = value;
+            }
+        }
+
+        public ICollection<string> Courses { get; set; }
+
+
+        //addCourse(course: String) : adds a course to the courses array.
+        //removeCourse(course: String): removes a course from the courses array.
+        //getCourses() -> String[]: returns the courses array.
+        //getAge() -> int: returns the age attribute.
+        //getName() -> String: returns the name attribute.
+
+        public void AddCourse(string course)
+        {
+            if (!this.Courses.Contains(course))
+            {
+                Courses.Add(course);
+
+                Console.WriteLine("Succesfuly added course");
+            }
+            else
+            {
+                Console.WriteLine("Already in this course !");
+            }
+        }
+
+        public void RemoveCourse(string course)
+        {
+            if (this.Courses.Contains(course))
+            {
+                Courses.Remove(course);
+
+                Console.WriteLine("Succesfuly removed course");
+            }
+            else
+            {
+                Console.WriteLine("There is no such course !");
+            }
+        }
+
+        public ICollection<string> GetCourses() => this.Courses;
+        public int GetAge() => this.Age;
+        public string GetName() => this.Name;
+    }
+}

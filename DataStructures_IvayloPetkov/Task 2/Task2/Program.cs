46c98a28fbed47deef8926b5497a3d662cf6a4ae
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-03 09:19:37 +0300

    Update Program.cs

--- DataStructures_IvayloPetkov/Task 2/Task2/Program.cs
+++ DataStructures_IvayloPetkov/Task 2/Task2/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Application
+{
+    class Program
+    {
+        static void phone_book()
+        {
+            string phones = "";
+            List<string> phoneNumbers = new List<string>();
+
+            Console.WriteLine("Enter phone numbers (empty line to stop):");
+            string phoneNumber = Console.ReadLine().Trim();
+            while (phoneNumber != "")
+            {
+                phoneNumbers.Add(phoneNumber);
+                phoneNumber = Console.ReadLine().Trim();
+            }
+
+            phoneNumbers.Insert(3, "+359 83 74 84 84");
+
+            Console.WriteLine("\nPhone book:");
+            foreach (string number in phoneNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            phoneNumbers.RemoveAt(2);
+
+            Console.WriteLine("\nPhone book after removing a phone number:");
+            foreach (string number in phoneNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    static void Main(string[] args)
+        {
+            phone_book();
+        }
+    }
+}

9fa6e4b1acd2e1599ba91ac3f84900101af69305
Dame Stoilovski <131446912+DameStoilovski99@users.noreply.github.com>
2023-05-04 20:39:05 +0300

    Bug fix

--- Loops_Dame_Stoilovski/src/ConcatenateTwoStrings.java
+++ Loops_Dame_Stoilovski/src/ConcatenateTwoStrings.java
@@ -5,24 +5,17 @@ public class ConcatenateTwoStrings {
 
         ArrayList<Character> result = new ArrayList<>();
 
-        if (firstString.length() > secondString.length()) {
-
-            for (int i = 0; i < secondString.length(); i++) {
-                result.add(firstString.charAt(i));
-                result.add(secondString.charAt(i));
-            }
-
-            addTheAdditionalCharacters(result, secondString, firstString);
-
-        } else {
-            for (int i = 0; i < firstString.length(); i++) {
-                result.add(firstString.charAt(i));
-                result.add(secondString.charAt(i));
-            }
-            if (result.size() < firstString.length() + secondString.length()) {
-                addTheAdditionalCharacters(result, firstString, secondString);
-            }
+        for (int i = 0; i < secondString.length(); i++) {
+            result.add(firstString.charAt(i));
+            result.add(secondString.charAt(i));
         }
+
+        if (firstString.length() > secondString.length()) {
+            addTheAdditionalCharacters(result, secondString, firstString);
+        } else {
+                addTheAdditionalCharacters(result, firstString, secondString);
+        }
+
         StringBuilder sb = new StringBuilder();
         for (Character c : result) {
             sb.append(c);

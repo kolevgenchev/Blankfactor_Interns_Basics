6be12761c4756fc1dd4ea7d0f8d4a4a3c2b22203
hristiyanaSh <hristiyana.shopova@blankfactor.com>
2023-05-05 09:28:22 +0300

    adding homework Task2 from Data Structures

---
+++ DataStructures_Hristiyana_Shopova/src/Task2.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Task2 {
+    public static void main(String[] args) {
+        LinkedList<String> phoneBook = new LinkedList<>();
+
+        phoneBook.add(0,"+359 86 74 84 84");
+        phoneBook.add(1,"+359 86 74 84 84");
+        phoneBook.add(2,"+359 84 74 84 84");
+        phoneBook.add(3,"+359 70 74 84 84");
+        phoneBook.add(4,"+359 83 74 84 84");
+        phoneBook.add(5,"+359 22 84 77 84");
+        phoneBook.add(6,"+359 30 74 84 84");
+
+
+        for (int i = 0; i < phoneBook.size(); i++) {
+            System.out.println(phoneBook.get(i));
+        }
+        phoneBook.remove(3);
+
+        System.out.println("---------");
+        for (int i = 0; i < phoneBook.size(); i++) {
+            System.out.println(phoneBook.get(i));
+        }
+    }
+}

b46a4c5d53618f5678d71c7ee8f6d9be7b340435
VladislavMilchov <vladislav.milchov@blankfactor.com>
2023-05-05 09:50:29 +0300

    DataStructures_VladislavMilchov

---
+++ DataStructures_VladislavMilchov/PhoneBook/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phoneBook = new List<string>();
+
+            phoneBook.Add("+359 31 52 32 13");
+            phoneBook.Add("+359 54 34 42 72");
+            phoneBook.Add("+359 23 11 26 22");
+            phoneBook.Add("+359 13 74 45 11");
+            phoneBook.Add("+359 93 74 84 74");
+            phoneBook.Add("+359 34 74 37 53");
+           
+            for (int i = 0; i < phoneBook.Count; i++)
+            {
+                if (i == 3)
+                {
+                    phoneBook.Insert(3, "+359 83 74 84 84");
+                }
+                
+                Console.WriteLine(phoneBook[i]);
+
+            }
+
+            phoneBook.RemoveAt(2);
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < phoneBook.Count; i++)
+            {    
+                Console.WriteLine(phoneBook[i]);
+            }
+            
+        }     
+    }
+}
---
+++ DataStructures_VladislavMilchov/Printer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Printer
+{
+    public static int CountUnfinishedTasks(int[] tasks, int[] papers)
+    {
+        int unfinishedTasks = 0;
+
+        for (int i = 0; i < tasks.Length; i++)
+        {          
+
+            for (int j = i + 1; j < tasks.Length; j++)
+            {
+                if (papers[j] == tasks[i])
+                {
+                    int temp = papers[i];
+                    papers[i] = papers[j];
+                    papers[j] = temp;
+                    break;
+                }
+            }
+
+            if (papers[i] != tasks[i])
+            {
+                unfinishedTasks++;
+            }
+        }
+
+        return unfinishedTasks;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] tasks = { 1, 1, 0, 1, 0, 1, 0, 1 , 1};
+        int[] papers = { 1, 1, 0, 0, 1, 1, 0, 0, 1};
+
+        int unfinishedTasks = Printer.CountUnfinishedTasks(tasks, papers);
+
+        Console.WriteLine("Number of unfinished tasks: " + unfinishedTasks);
+    }
+}
